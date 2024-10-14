4405223907c79085ae57bdd42deecf590eb7ff4e
hy_zhang <160694261+hengynzhang@users.noreply.github.com>
2024-10-14 18:19:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from '/src/App';
 import Modal from 'react-modal';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from 'src/reportWebVitals';
 
 Modal.setAppElement('#root');
 const root = ReactDOM.createRoot(document.getElementById('root'));
